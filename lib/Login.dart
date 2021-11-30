71b9990c742b45c8345a74c6e942c4faea62bd0e
s21myavuz <myavuz@sabanciuniv.edu>
2021-11-30 18:57:32 +0300

    Login'den geri yok

--- lib/Login.dart
+++ lib/Login.dart
@@ -43,112 +43,114 @@ class _LoginPage2 extends State<LoginPage> {
   }
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Form(
-              key: _formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
-                    Image.asset('assets/unite_logo.png', height: 150, width: 150,),
-                    SizedBox(height: 20.0,),
-                    Text("UNIte", style: AppStyles.appNameMainPage,),
-                    SizedBox(height: 20.0,),
-                    TextFormField(
-                      textAlign: TextAlign.center,
-                      decoration: new InputDecoration(
-                        hintText: "Enter Email",
-                        fillColor: Colors.black,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(0.0),
-                          borderSide: new BorderSide(),
-                        ),
-                      ),
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        else if(!EmailValidator.validate(value)){
-                          return 'Please enter a valid email address';
-                        }
-                        else {
-                          email = value;
-                        }
-                        return null;
-                      },
-
-                    ),
-                    SizedBox(height: 20.0,),
-                    TextFormField(
-                      obscureText: true,
-                      textAlign: TextAlign.center,
-                      decoration: new InputDecoration(
-                        hintText: "Enter Password",
-                        fillColor: Colors.black,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(0.0),
-                          borderSide: new BorderSide(),
-                        ),
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        else if (value.length < 6){
-                          return 'Password length cannot be less than 6 characters';
-                        }
-                        else if(!regex.hasMatch(value)){
-                          return 'Password should include an uppercase letter, a lowercase letter, \n one digit and a special character';
-                        }
-                        else{
-                          password = value;
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 20.0,),
-                    ElevatedButton(
-                      style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith(buttonColorCheck)),
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('Login Successful')),
-                          );
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => MainPage()),
-                          );
-                          setLoggedIn();
-                        }
-                      },
-                      child: const Text('Login', style: TextStyle(fontSize: 16),),
-                    ),
-                    SizedBox(height: 10.0,),
-                    Row(
+    return new WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          body: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("New to Unite? ", style: TextStyle(fontSize: 16),),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => RegisterPage()),
-                            );
+                      children:[
+                        Image.asset('assets/unite_logo.png', height: 150, width: 150,),
+                        SizedBox(height: 20.0,),
+                        Text("UNIte", style: AppStyles.appNameMainPage,),
+                        SizedBox(height: 20.0,),
+                        TextFormField(
+                          textAlign: TextAlign.center,
+                          decoration: new InputDecoration(
+                            hintText: "Enter Email",
+                            fillColor: Colors.black,
+                            border: new OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(0.0),
+                              borderSide: new BorderSide(),
+                            ),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            else if(!EmailValidator.validate(value)){
+                              return 'Please enter a valid email address';
+                            }
+                            else {
+                              email = value;
+                            }
+                            return null;
                           },
-                          child: new Text("Sign Up", style: AppStyles.signUp, ),
-                        )
+
+                        ),
+                        SizedBox(height: 20.0,),
+                        TextFormField(
+                          obscureText: true,
+                          textAlign: TextAlign.center,
+                          decoration: new InputDecoration(
+                            hintText: "Enter Password",
+                            fillColor: Colors.black,
+                            border: new OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(0.0),
+                              borderSide: new BorderSide(),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            else if (value.length < 6){
+                              return 'Password length cannot be less than 6 characters';
+                            }
+                            else if(!regex.hasMatch(value)){
+                              return 'Password should include an uppercase letter, a lowercase letter, \n one digit and a special character';
+                            }
+                            else{
+                              password = value;
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 20.0,),
+                        ElevatedButton(
+                          style: ButtonStyle(backgroundColor: MaterialStateProperty.resolveWith(buttonColorCheck)),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Login Successful')),
+                              );
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => MainPage()),
+                              );
+                              setLoggedIn();
+                            }
+                          },
+                          child: const Text('Login', style: TextStyle(fontSize: 16),),
+                        ),
+                        SizedBox(height: 10.0,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("New to Unite? ", style: TextStyle(fontSize: 16),),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => RegisterPage()),
+                                );
+                              },
+                              child: new Text("Sign Up", style: AppStyles.signUp, ),
+                            )
+                          ],
+                        ),
                       ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          )
-      ),
+              )
+          ),
+        ),
     );
-
   }
 }
